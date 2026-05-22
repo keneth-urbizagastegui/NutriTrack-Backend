0f3468ec7ff7ce67137187a3df9a42e57af21981
keneth <keneth.urbizagastegu@utec.edu.pe>
2026-05-22 17:10:29 -0500

    fix(s3): configure S3Client with DefaultCredentialsProvider to support session tokens and task IAM roles

--- src/main/java/pe/edu/utec/nutritrack/config/S3Config.java
+++ src/main/java/pe/edu/utec/nutritrack/config/S3Config.java
@@ -3,19 +3,12 @@ package pe.edu.utec.nutritrack.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-
-    @Value("${AWS_ACCESS_KEY_ID:dummy_access_key}")
-    private String accessKey;
-
-    @Value("${AWS_SECRET_ACCESS_KEY:dummy_secret_key}")
-    private String secretKey;
 
     @Value("${aws.s3.region:us-east-1}")
     private String region;
@@ -24,9 +17,7 @@ public class S3Config {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)
-                ))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
